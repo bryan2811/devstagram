ff10328cdc04c4bd78294119867cc38c99265526
Bryan <bryan@blacksmith.agency>
2024-02-04 17:05:25 -0300

    Update routes and layout for registration functionality

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,37 +5,106 @@
 @section('content')
     <div class="md:flex md:justify-center md:gap-10 md:items-center">
         <div class="p-5 md:w-6/12">
-            <img src="{{ asset('img/register.jpg') }}" alt="Register Image">
+            <img
+                src="{{ asset('img/register.jpg') }}"
+                alt="Register Image"
+            >
         </div>
 
         <div class="p-6 bg-white rounded-lg shadow-xl md:w-4/12">
-            <form>
+            <form
+                action="{{ route('register') }}"
+                method="POST"
+                novalidate
+            >
+                @csrf
                 <div class="mb-5">
-                    <label for="name" class="block mb-2 font-bold text-gray-500 uppercase">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Your Name" class="w-full p-3 border rounded-lg">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="name"
+                    >Name</label>
+                    <input
+                        class="w-full p-3 border rounded-lg @error('name') border-red-500 @enderror"
+                        id="name"
+                        name="name"
+                        type="text"
+                        value={{ old('name') }}
+                        placeholder="Your Name"
+                    >
+                    @error('name')
+                        <p class="p-2 my-2 text-center text-white bg-red-500 rounded-lg">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-5">
-                    <label for="username" class="block mb-2 font-bold text-gray-500 uppercase">Username</label>
-                    <input type="text" name="username" id="username" placeholder="Your Username" class="w-full p-3 border rounded-lg">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="username"
+                    >Username</label>
+                    <input
+                        class="w-full p-3 border rounded-lg @error('username') border-red-500 @enderror"
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Your Username"
+                    >
+                    @error('username')
+                        <p class="p-2 my-2 text-center text-white bg-red-500 rounded-lg">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-5">
-                    <label for="email" class="block mb-2 font-bold text-gray-500 uppercase">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your Email" class="w-full p-3 border rounded-lg">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="email"
+                    >Email</label>
+                    <input
+                        class="w-full p-3 border rounded-lg @error('email') border-red-500 @enderror"
+                        id="email"
+                        name="email"
+                        type="text"
+                        placeholder="Your Email"
+                    >
+                    @error('email')
+                        <p class="p-2 my-2 text-center text-white bg-red-500 rounded-lg">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-5">
-                    <label for="password" class="block mb-2 font-bold text-gray-500 uppercase">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Your Password" class="w-full p-3 border rounded-lg">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="password"
+                    >Password</label>
+                    <input
+                        class="w-full p-3 border rounded-lg @error('password') border-red-500 @enderror"
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Your Password"
+                    >
+                    @error('password')
+                        <p class="p-2 my-2 text-center text-white bg-red-500 rounded-lg">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-5">
-                    <label for="password_confirmation" class="block mb-2 font-bold text-gray-500 uppercase">Confirm Password</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password" class="w-full p-3 border rounded-lg">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="password_confirmation"
+                    >Confirm Password</label>
+                    <input
+                        class="w-full p-3 border rounded-lg"
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Confirm Password"
+                    >
                 </div>
 
-                <button type="submit" class="w-full p-3 font-bold text-white uppercase transition-colors rounded-lg cursor-pointer bg-sky-600 hover:bg-sky-700">Create Account</button>
+                <button
+                    class="w-full p-3 font-bold text-white uppercase transition-colors rounded-lg cursor-pointer bg-sky-600 hover:bg-sky-700"
+                    type="submit"
+                >Create Account</button>
             </form>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>DevStagram - @yield('title')</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet">
-        @vite('resources/css/app.css')
-    </head>
-    <body class="bg-gray-100">
-        <header class="p-5 bg-white border-b shadow">
-            <div class="container flex items-center justify-between mx-auto">
-                <h1 class="text-lg font-black md:text-3xl">
-                    DevStagram
-                </h1>
 
-                <nav class="flex gap-2 items-center *:font-bold *:uppercase *:text-gray-600 *:text-sm *:sm:text-base">
-                    <a href="#">Login</a>
-                    <a href="/create-account">Create Account</a>
-                </nav>
-            </div>
-        </header>
+<head>
+    <meta charset="utf-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
+    <title>DevStagram - @yield('title')</title>
+    <link
+        href="https://fonts.googleapis.com"
+        rel="preconnect"
+    >
+    <link
+        href="https://fonts.gstatic.com"
+        rel="preconnect"
+        crossorigin
+    >
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap"
+        rel="stylesheet"
+    >
+    @vite('resources/css/app.css')
+</head>
 
-        <main class="container mx-auto mt-10">
-            <h2 class="mb-10 text-3xl font-black text-center">
-                @yield('title')
-            </h2>
+<body class="bg-gray-100">
+    <header class="p-5 bg-white border-b shadow">
+        <div class="container flex items-center justify-between mx-auto">
+            <h1 class="text-lg font-black md:text-3xl">
+                DevStagram
+            </h1>
 
-            @yield('content')
-        </main>
+            <nav class="flex gap-2 items-center *:font-bold *:uppercase *:text-gray-600 *:text-sm *:sm:text-base">
+                <a href="#">Login</a>
+                <a href="{{ route('register') }}">Create Account</a>
+            </nav>
+        </div>
+    </header>
 
-        <footer class="p-5 mt-10 font-bold text-center text-gray-500 uppercase">
-            DevStagram - All rights reserved &copy; {{ date('Y') }}
-        </footer>
-    </body>
+    <main class="container mx-auto mt-10">
+        <h2 class="mb-10 text-3xl font-black text-center">
+            @yield('title')
+        </h2>
+
+        @yield('content')
+    </main>
+
+    <footer class="p-5 mt-10 font-bold text-center text-gray-500 uppercase">
+        DevStagram - All rights reserved &copy; {{ date('Y') }}
+    </footer>
+</body>
+
 </html>
