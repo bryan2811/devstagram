212249dbec61019af68c2b3566cf6a34fa9b1abf
Bryan <bryan@blacksmith.agency>
2024-02-04 02:02:19 -0300

    [WIP] Initial layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,5 @@
 @extends('layouts.app')
 
 @section('title', 'Home')
+
+@section('content', 'Welcome to DevStagram')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
                     DevStagram
                 </h1>
 
-                <nav class="flex gap-2 items-center *:font-bold *:uppercase *:text-gray-600 *:text-base">
+                <nav class="flex gap-2 items-center *:font-bold *:uppercase *:text-gray-600">
                     <a href="#">Login</a>
                     <a href="#">Create Account</a>
                 </nav>
@@ -21,13 +21,15 @@
         </header>
 
         <main class="container mx-auto mt-10">
-            <h2 class="mb-10 text-3xl font-black text-center text-">
+            <h2 class="mb-10 text-3xl font-black text-center">
                 @yield('title')
             </h2>
 
             @yield('content')
         </main>
 
-        <footer></footer>
+        <footer class="p-5 font-bold text-center text-gray-500 uppercase">
+            DevStagram - All rights reserved &copy; {{ date('Y') }}
+        </footer>
     </body>
 </html>
