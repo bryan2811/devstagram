ec9c7e71be7df75d91dd9fed9bb769d5030cf3c0
Bryan <bryan@blacksmith.agency>
2024-02-04 17:05:08 -0300

    Add form validation to the store method in RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,9 +6,19 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
     public function index()
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        // Form Validation
+        $this->validate($request, [
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required'
+        ]);
     }
 }
