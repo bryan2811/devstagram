a8da992e8ee1e0178344328369e59a9e0adf12e9
Bryan <bryan@blacksmith.agency>
2024-02-05 14:45:13 -0300

    Add Dropzone file upload functionality and update PostController and app.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone("#dropzone", {
+    dictDefaultMessage: "Drop files here to upload",
+    acceptedFiles: ".jpg,.png,.jpeg,.gif",
+    addRemoveLinks: true,
+    dictRemoveFile: "Remove",
+    maxFilesize: 1,
+    uploadMultiple: false,
+});
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('title', 'Create Post - DevStagram')
+
+@section('content')
+    <div class="md:flex md:items-center">
+        <div class="px-10 md:w-1/2">
+            <form
+                action="/img"
+                id="dropzone"
+                class="flex flex-col items-center justify-center w-full border-2 border-dashed rounded dropzone h-96"
+            ></form>
+        </div>
+
+        <div class="p-10 mt-10 bg-white rounded-lg shadow-xl md:w-1/2 md:mt-0">
+            <form
+                action="{{ route('register') }}"
+                method="POST"
+                novalidate
+            >
+                @csrf
+                <div class="mb-5">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="title"
+                    >Title</label>
+                    <input
+                        id="title"
+                        class="w-full p-3 border rounded-lg @error('title') border-red-500 @enderror"
+                        name="title"
+                        type="text"
+                        placeholder="Post Title"
+                        value={{ old('title') }}
+                    >
+                    @error('title')
+                        <p class="p-2 my-2 text-center text-white bg-red-500 rounded-lg">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label
+                        class="block mb-2 font-bold text-gray-500 uppercase"
+                        for="description"
+                    >Description</label>
+                    <textarea
+                        id="description"
+                        class="w-full p-3 border rounded-lg @error('description') border-red-500 @enderror"
+                        name="description"
+                        type="text"
+                        placeholder="Post Description"
+                        value={{ old('description') }}
+                    >{{ old('title') }}</textarea>
+                    @error('description')
+                        <p class="p-2 my-2 text-center text-white bg-red-500 rounded-lg">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <button
+                    class="w-full p-3 font-bold text-white uppercase transition-colors rounded-lg cursor-pointer bg-sky-600 hover:bg-sky-700"
+                    type="submit"
+                >Create Post
+                </button>
+            </form>
+        </div>
+    </div>
+@endsection
